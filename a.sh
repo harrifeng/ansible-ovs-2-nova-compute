dabb8353bf3a836911cf59093915962de13e1589
root <root@ovs-compute-15.(none)>
2017-05-22 10:36:55 +0000

    add context 'modify hostname'

--- a.sh
+++ a.sh
@@ -3,12 +3,12 @@
 
 
 ## TOTAL
-ansible-playbook -i hosts   site.yml
+#ansible-playbook -i hosts   site.yml
 
 
 ## per roles
 #ansible-playbook -i hosts   run_role.yml   -e "ROLE=docker-stop"  -e "TARGET=localhost"
-#ansible-playbook -i hosts   run_role.yml   -e "ROLE=common"       -e "TARGET=localhost"
+ansible-playbook -i hosts   run_role.yml   -e "ROLE=common"       -e "TARGET=localhost"
 #ansible-playbook -i hosts   run_role.yml   -e "ROLE=network"      -e "TARGET=localhost"
 #ansible-playbook -i hosts   run_role.yml   -e "ROLE=config-nova"  -e "TARGET=localhost"
 #ansible-playbook -i hosts   run_role.yml   -e "ROLE=docker-start" -e "TARGET=localhost"
